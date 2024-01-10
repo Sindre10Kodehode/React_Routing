e22b06ea911f9267454c53a4bf9e63b2f3291b61
Sindre10Kodehode <sindre10kodehode@gmail.com>
2024-01-10 12:23:45 +0100

    *

--- src/page/ToDo.jsx
+++ src/page/ToDo.jsx
@@ -15,20 +15,13 @@ export const ToDos = () => {
     fetchTodo();
   }, []);
 
-
-
   return <>
         <section className="flex flex-col items-center bg-slate-200">
         <h1 className="mt-8 text-2xl">
           På denne siden har jeg fetchet data fra en API.
         </h1>
         <p>Her har du en liste av "kommentarer" hentet fra jsonplaceholder.</p>
-        {/* <input
-          id="comSearch"
-          type="search"
-          placeholder="Search Here!"
-          className="pl-4 m-4 border-2 rounded-xl"
-        /> */}
+
         {data.map((post) => (
           <div
             className="flex flex-col justify-center w-3/5 p-4 m-8 rounded-2xl bg-slate-300"
@@ -38,10 +31,10 @@ export const ToDos = () => {
               <span className="font-bold">To Do List Number: </span> {post.id}
             </h1>
             <h1 className="text-xl">
-              <span className="font-bold">Name of item: </span> {post.title}
+              <span className="font-bold">To Do: </span> {post.title}
             </h1>
             <p>
-              <span className="font-bold">COMPLETED? </span> {post.completed ? "❌" : "✔️"}
+              <span className="font-bold">STATUS: </span> {post.completed ? "Unfinished ❌" : "Finished ✔️"}
             </p>
           </div>
         ))}

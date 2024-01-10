43e91322269bc45a095748629db3b819e183269e
Sindre10Kodehode <sindre10kodehode@gmail.com>
2024-01-10 15:00:56 +0100

    *

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,8 +1,8 @@
 export const MainPage = () => {
   return (
     <>
-      <section className="flex justify-center p-8 bg-slate-200 Hero font-Ubuntu2">
-        <div className="flex">
+      <section className="flex justify-center p-8 bg-slate-200 Hero">
+        <div className="flex font-medium">
           <img src="/src/assets/react.svg" className="animate-spin-slow" />
         <h1 className="p-4 text-2xl">Dette er en nettside laget med REACT</h1>
         <img src="/src/assets/react.svg" className="animate-spin-slow" />
